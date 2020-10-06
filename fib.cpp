2650c610d1cfb6aec2108c71889e745b8665cdcd
JianRon <2538094841@qq.com>
2020-10-06 14:42:17 +0800

    Create fib.cpp

---
+++ fib.cpp
@@ -0,0 +1,10 @@
+int fib(ll n) {
+    ll x = 0, y = 1, t;
+    for(int d = 62 - builtin_clzll(++n);d >= 0;d--) {
+        t = (x * x + y * y) % p;
+        y = (x + x + y) % p;
+        x = t;
+        if(n >> d & 1) x = y, y = (y + t) % p;
+     }
+     return x;
+}
